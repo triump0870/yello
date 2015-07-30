c509db09b167d7c7f55c521ae137919865f5d338
Rohan Roy <b4you0870@gmail.com>
2015-07-30 20:29:23 +0530

    Updated

--- yello/settings.py
+++ yello/settings.py
@@ -87,7 +87,8 @@ SOCIAL_AUTH_PIPELINE = (
     'social_auth.backends.pipeline.user.create_user',
     'social_auth.backends.pipeline.social.associate_user',
     'social_auth.backends.pipeline.social.load_extra_data',
-    'social_auth.backends.pipeline.user.update_user_details'
+    'social_auth.backends.pipeline.user.update_user_details',
+    'social_auth.backends.pipeline.associate.associate_by_email',
 )
 
 
